9bbe3527d4bf2b3fe017c51776e9df1dcf5e80a0
mrdcvlsc <mrdcvlsc@gmail.com>
2024-12-06 19:06:52 +0800

    added DaySchedule struct and methods

---
+++ GeneticAlgorithm/DaySchedule.go
@@ -0,0 +1,18 @@
+package geneticalgorithm
+
+import (
+	"log"
+)
+
+type DaySchedule [DAILY_TIME_SLOTS]TimeSlot
+
+func (day_schedule *DaySchedule) GetTimeSlot(time_slot_idx int) *TimeSlot {
+	if time_slot_idx < 0 || time_slot_idx >= DAILY_TIME_SLOTS {
+		log.Fatalf(
+			"GetTimeSlot(time_slot_idx = %d | min:max = 0:%d): error index out of bounds",
+			time_slot_idx, DAILY_TIME_SLOTS,
+		)
+	}
+
+	return &(*day_schedule)[time_slot_idx]
+}
