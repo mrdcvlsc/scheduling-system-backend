a09d7f2f5dd7c59501025824f6121e824f7d539c
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-09 19:31:36 +0800

    add initial genetic algorithm source files

---
+++ GeneticAlgorithm/GeneratePopulations.go
@@ -0,0 +1,16 @@
+package geneticalgorithm
+
+import (
+	// "fmt"
+	// "testing"
+
+	// storage "github.com/mrdcvlsc/scheduling-system-backend/Storage"
+	sd "github.com/mrdcvlsc/scheduling-system-backend/Resources/ScheduleDSA"
+)
+
+func NewPopulation() sd.SchedulesInUniversity {
+	// TODO: get all courses curriculum in the university
+	// TODO: get all sections / classes in the university
+	// TODO: get all subjects
+	return nil
+}
