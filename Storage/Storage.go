59dfe5371633055d995c776ad9ef49a7be6440cd
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-11 13:59:29 +0800

    added method to storage interface

--- Storage/Storage.go
+++ Storage/Storage.go
@@ -7,11 +7,13 @@ import (
 	// storage "github.com/mrdcvlsc/scheduling-system-backend/Storage"
 	curriculum "github.com/mrdcvlsc/scheduling-system-backend/Resources/Curriculum"
 	instructor "github.com/mrdcvlsc/scheduling-system-backend/Resources/Instructors"
+	room "github.com/mrdcvlsc/scheduling-system-backend/Resources/Rooms"
 )
 
 type persistenceRepository interface {
 	GetAllCurriculum() []curriculum.Curriculum
 	GetAllInstructors() []instructor.Instructor
+	GetAllRooms() []room.Room
 }
 
 type PersistenceService struct {
