3564794180d9ee7d47b1b59481c0cd7b710623aa
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-11 13:58:51 +0800

    add room struct type

--- Resources/Rooms/Rooms.go
+++ Resources/Rooms/Rooms.go
@@ -1,1 +1,9 @@
-package geneticalgorithm
+package rooms
+
+type Room struct {
+	RoomID       uint16
+	DepartmentID uint16 `json:"DepartmentID"`
+	Capacity     uint16 `json:"Capacity"`
+	RoomType     uint32 `json:"RoomType"`
+	Name         string `json:"Name"`
+}
