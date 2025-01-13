4e528b6f4340d6fe076ad452b9bf2b7ee3013681
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-13 12:24:44 +0800

    added comments in rooms.go

--- Resources/Rooms/Rooms.go
+++ Resources/Rooms/Rooms.go
@@ -3,7 +3,7 @@ package rooms
 type Room struct {
 	RoomID       uint16
 	DepartmentID uint16 `json:"DepartmentID"`
-	Capacity     uint16 `json:"Capacity"`
-	RoomType     uint32 `json:"RoomType"`
+	Capacity     uint16 `json:"Capacity"` // Maximum numbers of section allocation in a room
+	RoomType     uint16 `json:"RoomType"` // Determines the room types.
 	Name         string `json:"Name"`
 }
