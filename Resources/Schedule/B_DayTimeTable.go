12d8c5fed9fdb261bfcf9b0689c4bbb8aa759e59
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-18 19:01:27 +0800

    updated DayTimeTable.Availability method
    * renamed `hours` parameter to `time_slot_size` since using `hours` is semantically wrong
    
    * add panic the `time_slot_idx` + `time_slot_size` exceeds the daily time slot limit
    
    * add tests for the DayTimeTable.Availability

--- Resources/Schedule/B_DayTimeTable.go
+++ Resources/Schedule/B_DayTimeTable.go
@@ -20,25 +20,28 @@ func (day *DayTimeTable) Get(time_slot_idx int) *TimeSlot {
 	return &(*day)[time_slot_idx]
 }
 
-func (day *DayTimeTable) Availability(time_slot_idx, hours int) bool {
+func (day *DayTimeTable) Availability(time_slot_idx, time_slot_size int) bool {
 	if time_slot_idx < 0 || time_slot_idx >= N_DAILY_TIME_SLOTS {
 		panic(fmt.Sprintf(
-			"GetTimeSlot(time_slot_idx = %d | min:max = 0:%d): error index out of bounds",
+			"GetTimeSlot(time_slot_idx = %d | min:max = 0:%d): error `time_slot_idx` out of bounds",
 			time_slot_idx, N_DAILY_TIME_SLOTS,
 		))
 	}
 
-	if (time_slot_idx + hours - 1) >= N_DAILY_TIME_SLOTS {
-		return false
+	if (time_slot_idx + time_slot_size - 1) >= N_DAILY_TIME_SLOTS {
+		panic(fmt.Sprintf(
+			"GetTimeSlot(time_slot_size = %d + time_slot_idx = %d): error `time_slot_size + time_slot_idx` out of bounds",
+			time_slot_size, time_slot_idx,
+		))
 	}
 
 	var counted_time_slots int
-	for counted_time_slots = 0; counted_time_slots < hours; counted_time_slots++ {
+
+	for counted_time_slots = 0; counted_time_slots < time_slot_size; counted_time_slots++ {
 		if day[time_slot_idx+counted_time_slots].GetSubjectID() != 0 {
 			return false
 		}
 	}
 
-	// sanity check, just return true later if there is no problem
-	return counted_time_slots == hours
+	return counted_time_slots == time_slot_size
 }
