148d37f5b70d9f81a6842c1ffb32451d46a3263d
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-19 15:19:42 +0800

    use uni sched validation methods in tests

--- GeneticAlgorithm/GeneratePopulations_test.go
+++ GeneticAlgorithm/GeneratePopulations_test.go
@@ -1,6 +1,7 @@
 package geneticalgorithm_test
 
 import (
+	"fmt"
 	"testing"
 
 	GA "github.com/mrdcvlsc/scheduling-system-backend/GeneticAlgorithm"
@@ -9,17 +10,66 @@ import (
 func TestNewPopulation(t *testing.T) {
 
 	println("1st Semester Information:")
-	GA.NewIndividual(GA.TERM_1ST_SEMESTER, 0)
+	uni_time_table1, _ := GA.NewIndividual(GA.TERM_1ST_SEMESTER, 0)
 
 	println("2nd Semester Information:")
-	GA.NewIndividual(GA.TERM_2ND_SEMESTER, 0)
+	uni_time_table2, _ := GA.NewIndividual(GA.TERM_2ND_SEMESTER, 0)
 
-	// if available == false {
-	// 	t.Errorf(
-	// 		"GetAvailability(day = %d, time_slot = %d) : {loop test phase 4} should not be false again",
-	// 		day, time_slot,
-	// 	)
-	// }
+	fmt.Println("=================================")
+
+	fmt.Print("\nTime Slot section(0) BEFORE:\n")
+	fmt.Printf("%+v", uni_time_table1.Get(0).Get(5).Get(23))
+	fmt.Print("\n\n")
+
+	fmt.Print("\nTime Slot section(1) BEFORE:\n")
+	fmt.Printf("%+v", uni_time_table1.Get(1).Get(5).Get(23))
+	fmt.Print("\n\n")
+
+	fmt.Print("\nTime Slot section(2) BEFORE:\n")
+	fmt.Printf("%+v", uni_time_table1.Get(2).Get(5).Get(23))
+	fmt.Print("\n\n")
+
+	fmt.Println("=================================")
+
+	if err := uni_time_table1.Get(0).Get(5).Get(23).SetInstructorID(33); err != nil {
+		t.Error(err)
+	}
+
+	if err := uni_time_table1.Get(7).Get(5).Get(23).SetInstructorID(33); err != nil {
+		t.Error(err)
+	}
+
+	if err := uni_time_table1.Get(2).Get(5).Get(23).SetInstructorID(33); err != nil {
+		fmt.Println(err.Error())
+		t.Error(err)
+	}
+
+	fmt.Println("=================================")
+
+	fmt.Print("\nTime Slot section(0) AFTER:\n")
+	fmt.Printf("%+v", uni_time_table1.Get(0).Get(5).Get(23))
+	fmt.Print("\n\n")
+
+	fmt.Print("\nTime Slot section(1) AFTER:\n")
+	fmt.Printf("%+v", uni_time_table1.Get(1).Get(5).Get(23))
+	fmt.Print("\n\n")
+
+	fmt.Print("\nTime Slot section(2) AFTER:\n")
+	fmt.Printf("%+v", uni_time_table1.Get(2).Get(5).Get(23))
+	fmt.Print("\n\n")
+
+	uni1errs := uni_time_table1.Validate()
+	uni2errs := uni_time_table2.Validate()
+
+	fmt.Println("=================================")
+
+	for _, err := range uni1errs {
+		t.Error(err)
+	}
+
+	for _, err := range uni2errs {
+		t.Error(err)
+	}
 }
 
 func BenchmarkNewPopulationSem1(b *testing.B) {
