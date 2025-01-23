23701b66da920a108ed658bcc4f0ca23d18678c4
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-23 19:00:36 +0800

    test for gym type subjects in json file persistence

--- Storage/JsonFilePersistence_test.go
+++ Storage/JsonFilePersistence_test.go
@@ -2,6 +2,7 @@ package Storage_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/mrdcvlsc/scheduling-system-backend/Storage"
@@ -12,13 +13,45 @@ var TestPersistence Storage.PersistenceService
 
 func TestJsonFilePersistence_GetAllSubjects(t *testing.T) {
 	TestPersistence.Service = &Storage.JsonFilePersistence{}
-	curriculums := TestPersistence.Service.GetAllSubjects()
-	Utils.PrettyPrint(curriculums)
+	subjects := TestPersistence.Service.GetAllSubjects()
+
+	for _, subject := range subjects {
+		if strings.Contains(subject.Code, "FITT") {
+			if ((subject.BitFlags & 1) != 1) || !subject.IsGymType() {
+				t.Errorf("Subject %s - should have one 1 bit in the least significant bit", subject.Code)
+			}
+		} else {
+			if ((subject.BitFlags & 1) != 0) || subject.IsGymType() {
+				t.Errorf("Subject %s - should have a 0 bit in the least significant bit", subject.Code)
+			}
+		}
+	}
+
+	Utils.PrettyPrint(subjects)
 }
 
 func TestJsonFilePersistence_GetAllCurriculum(t *testing.T) {
 	TestPersistence.Service = &Storage.JsonFilePersistence{}
 	curriculums := TestPersistence.Service.GetAllCurriculum()
+
+	for _, curriculum := range curriculums {
+		for _, year_level := range curriculum.YearLevels {
+			for _, semester := range year_level.Semesters {
+				for _, subject := range semester.Subjects {
+					if strings.Contains(subject.Code, "FITT") {
+						if ((subject.BitFlags & 1) != 1) || !subject.IsGymType() {
+							t.Errorf("Subject %s - should have one 1 bit in the least significant bit", subject.Code)
+						}
+					} else {
+						if ((subject.BitFlags & 1) != 0) || subject.IsGymType() {
+							t.Errorf("Subject %s - should have a 0 bit in the least significant bit", subject.Code)
+						}
+					}
+				}
+			}
+		}
+	}
+
 	Utils.PrettyPrint(curriculums)
 }
 
