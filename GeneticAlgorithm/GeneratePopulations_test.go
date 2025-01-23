e1c453106b07dee437dd81fa171ec3832fa0ded8
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-23 23:20:57 +0800

    modified: GeneticAlgorithm/GeneratePopulations_test.go

--- GeneticAlgorithm/GeneratePopulations_test.go
+++ GeneticAlgorithm/GeneratePopulations_test.go
@@ -7,70 +7,37 @@ import (
 	GA "github.com/mrdcvlsc/scheduling-system-backend/GeneticAlgorithm"
 )
 
-func TestNewPopulation(t *testing.T) {
+func TestNewPopulationEnoughRooms(t *testing.T) {
+
+	fmt.Print("\n################ First Semester ################ \n\n")
+	GA.EstimateResourceAvailability(GA.TERM_1ST_SEMESTER, 0)
+
+	// fmt.Print("\n################ Second Semester ################ \n\n")
+	// GA.EstimateResourceAvailability(1, 0)
+}
+
+func TestNewPopulationFirstSem(t *testing.T) {
 
 	println("1st Semester Information:")
 	uni_time_table1, _ := GA.NewIndividual(GA.TERM_1ST_SEMESTER, 0)
-
-	println("2nd Semester Information:")
-	uni_time_table2, _ := GA.NewIndividual(GA.TERM_2ND_SEMESTER, 0)
-
-	fmt.Println("=================================")
-
-	fmt.Print("\nTime Slot section(0) BEFORE:\n")
-	fmt.Printf("%+v", uni_time_table1.Get(0).Get(5).Get(23))
-	fmt.Print("\n\n")
-
-	fmt.Print("\nTime Slot section(1) BEFORE:\n")
-	fmt.Printf("%+v", uni_time_table1.Get(1).Get(5).Get(23))
-	fmt.Print("\n\n")
-
-	fmt.Print("\nTime Slot section(2) BEFORE:\n")
-	fmt.Printf("%+v", uni_time_table1.Get(2).Get(5).Get(23))
-	fmt.Print("\n\n")
-
-	fmt.Println("=================================")
-
-	// if err := uni_time_table1.Get(0).Get(5).Get(23).SetInstructorID(33); err != nil {
-	// 	t.Error(err)
-	// }
-
-	// if err := uni_time_table1.Get(7).Get(5).Get(23).SetInstructorID(33); err != nil {
-	// 	t.Error(err)
-	// }
-
-	// if err := uni_time_table1.Get(2).Get(5).Get(23).SetInstructorID(33); err != nil {
-	// 	fmt.Println(err.Error())
-	// 	t.Error(err)
-	// }
-
-	fmt.Println("=================================")
-
-	fmt.Print("\nTime Slot section(0) AFTER:\n")
-	fmt.Printf("%+v", uni_time_table1.Get(0).Get(5).Get(23))
-	fmt.Print("\n\n")
-
-	fmt.Print("\nTime Slot section(1) AFTER:\n")
-	fmt.Printf("%+v", uni_time_table1.Get(1).Get(5).Get(23))
-	fmt.Print("\n\n")
-
-	fmt.Print("\nTime Slot section(2) AFTER:\n")
-	fmt.Printf("%+v", uni_time_table1.Get(2).Get(5).Get(23))
-	fmt.Print("\n\n")
-
 	uni1errs := uni_time_table1.Validate()
-	uni2errs := uni_time_table2.Validate()
-
-	fmt.Println("=================================")
 
 	for _, err := range uni1errs {
 		t.Error(err)
 	}
-
-	for _, err := range uni2errs {
-		t.Error(err)
-	}
 }
+
+// func TestNewPopulationSecondSem(t *testing.T) {
+
+// 	println("2nd Semester Information:")
+// 	uni_time_table2, _ := GA.NewIndividual(GA.TERM_2ND_SEMESTER, 0)
+
+// 	uni2errs := uni_time_table2.Validate()
+
+// 	for _, err := range uni2errs {
+// 		t.Error(err)
+// 	}
+// }
 
 func BenchmarkNewPopulationSem1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -78,8 +45,8 @@ func BenchmarkNewPopulationSem1(b *testing.B) {
 	}
 }
 
-func BenchmarkNewPopulationSem2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		GA.NewIndividual(GA.TERM_2ND_SEMESTER, 0)
-	}
-}
+// func BenchmarkNewPopulationSem2(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		GA.NewIndividual(GA.TERM_2ND_SEMESTER, 0)
+// 	}
+// }
