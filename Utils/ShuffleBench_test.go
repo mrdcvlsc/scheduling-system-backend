4b27e8f6e7ccc88bb26425f6d63aaac85abf7286
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-25 01:25:33 +0800

    removed repeating benchmarks

--- Utils/ShuffleBench_test.go
+++ Utils/ShuffleBench_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func BenchmarkShuffleIntnDedicated(b *testing.B) {
+func BenchmarkShuffleDedicated(b *testing.B) {
 	arr := make([]uint64, 0, 8)
 
 	for i := 0; i < 10; i++ {
@@ -21,66 +21,7 @@ func BenchmarkShuffleIntnDedicated(b *testing.B) {
 	}
 }
 
-func BenchmarkShuffleIntnDefault(b *testing.B) {
-	arr := make([]uint64, 0, 8)
-
-	for i := 0; i < 10; i++ {
-		arr = append(arr, uint64(i+1))
-	}
-
-	for i := 0; i < b.N; i++ {
-		rand.Shuffle(len(arr), func(i, j int) {
-			arr[i], arr[j] = arr[j], arr[i]
-		})
-	}
-}
-
-func BenchmarkShuffleInt63nDedicated(b *testing.B) {
-	arr := make([]uint64, 0, 8)
-
-	for i := 0; i < 10; i++ {
-		arr = append(arr, uint64(i+1))
-	}
-
-	rng := rand.New(rand.NewSource(time.Now().UnixMilli()))
-	for i := 0; i < b.N; i++ {
-		rng.Shuffle(len(arr), func(i, j int) {
-			arr[i], arr[j] = arr[j], arr[i]
-		})
-	}
-}
-
-func BenchmarkShuffleInt63nDefault(b *testing.B) {
-	arr := make([]uint64, 0, 8)
-
-	for i := 0; i < 10; i++ {
-		arr = append(arr, uint64(i+1))
-	}
-
-	for i := 0; i < b.N; i++ {
-		rand.Shuffle(len(arr), func(i, j int) {
-			arr[i], arr[j] = arr[j], arr[i]
-		})
-	}
-}
-
-func BenchmarkShuffleInt31nDedicated(b *testing.B) {
-	arr := make([]uint64, 0, 8)
-
-	for i := 0; i < 10; i++ {
-		arr = append(arr, uint64(i+1))
-	}
-
-	// fastest in my machine
-	rng := rand.New(rand.NewSource(time.Now().UnixMilli()))
-	for i := 0; i < b.N; i++ {
-		rng.Shuffle(len(arr), func(i, j int) {
-			arr[i], arr[j] = arr[j], arr[i]
-		})
-	}
-}
-
-func BenchmarkShuffleInt31nDefault(b *testing.B) {
+func BenchmarkShuffleDefault(b *testing.B) {
 	arr := make([]uint64, 0, 8)
 
 	for i := 0; i < 10; i++ {
