dca5e5e81ca76ccf0bcb8e7f44d6c7656264cdca
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-29 15:39:17 +0800

    add benchmark for rng with time complexity of generating new individual

--- Utils/RngBench_test.go
+++ Utils/RngBench_test.go
@@ -1,9 +1,12 @@
 package Utils
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/mrdcvlsc/scheduling-system-backend/Resources/Const"
 )
 
 func BenchmarkRngIntnDedicated(b *testing.B) {
@@ -43,5 +46,50 @@ func BenchmarkRngInt31nDedicated(b *testing.B) {
 func BenchmarkRngInt31nDefault(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		rand.Int31n(10_000_000)
+	}
+}
+
+// max number of curriculums
+const C int = 60
+
+// max number year level out of all curriculums / courses.
+const Y int = 5
+
+// max number of section per year level.
+const X int = 20
+
+// max number of subjects in a semester.
+const S int = 22
+
+// total time slot in a week
+const T int = Const.N_WEEKLY_TIME_SLOTS
+
+// max number of instructor per department
+const I int = 80
+
+// max number of a specific room type per department
+const R = 80
+
+// time complexity
+const O int = C * Y * X * S * 2 * T * (I + R)
+
+func BenchmarkEstimatedGenPopTimeComplexity(b *testing.B) {
+	rng := rand.New(rand.NewSource(time.Now().UnixMilli()))
+
+	fmt.Printf(
+		"Time Complexity | O( C * Y * X * S * 2 * T * (I + R) : O(%d * %d * %d * %d * 2 * %d * (%d + %d)) = %d\n",
+		C, Y, X, S, T, I, R, O,
+	)
+
+	total := uint64(0)
+	for i := 0; i < b.N; i++ {
+		for gen_pop_op := 0; gen_pop_op < O; gen_pop_op++ {
+
+			if (gen_pop_op % 1_000_000_000) == 0 {
+				fmt.Printf("iter: %d, gen_pop_op: %d\n", i, gen_pop_op)
+			}
+
+			total += uint64(rng.Int31n(10_000_000))
+		}
 	}
 }
