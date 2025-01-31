7e5f925f6ce4f6b20b55491d675f837cc0e4275b
mrdcvlsc <mrdcvlsc@gmail.com>
2025-01-31 20:48:08 +0800

    use query parameter validation methods to GetClassSchedule & GetUniversitySchedule

--- Routes/RoutesV1/GetClassSchedule.go
+++ Routes/RoutesV1/GetClassSchedule.go
@@ -2,10 +2,8 @@ package RoutesV1
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/mrdcvlsc/scheduling-system-backend/RouteGlobals"
 	"github.com/mrdcvlsc/scheduling-system-backend/Schedule"
 )
 
@@ -22,7 +20,7 @@ func GetClassSchedule(ctx *gin.Context) {
 
 	// parse department_id parameter
 
-	// TODO: use this for authentication later on.
+	// TODO: use department_id for authentication later on.
 
 	_, is_valid_department_id_param := IsValidParameterDepartmentID(ctx)
 
@@ -32,48 +30,17 @@ func GetClassSchedule(ctx *gin.Context) {
 
 	// load university schedules
 
-	university_schedules, read_err := RouteGlobals.SchedulePersistence.ReaderService.LoadSchedules(semester)
+	university_schedules, has_obtained := ObtainUniversitySchedule(ctx, semester)
 
-	if read_err != nil {
-		ctx.String(http.StatusInternalServerError, "error reading schedule")
+	if !has_obtained {
 		return
-	}
-
-	if university_schedules.IsEmpty() {
-		ctx.String(http.StatusNotFound, "schedule was not found, please generate one first")
-		return
-	}
-
-	for _, validation_err := range university_schedules.Validate() {
-		if validation_err != nil {
-			ctx.String(http.StatusConflict, "server detected an invalid schedule")
-			return
-		}
 	}
 
 	// parse schedule_idx parameter
 
-	param_schedule_idx := ctx.Query("schedule_idx")
+	schedule_idx, is_valid_idx := IsValidUniversityScheduleIndex(ctx, university_schedules)
 
-	if param_schedule_idx == "" {
-		ctx.String(http.StatusBadRequest, "mising 'schedule_idx' parameter or parameter value")
-		return
-	}
-
-	schedule_idx, schedule_idx_atoi_err := strconv.Atoi(param_schedule_idx)
-
-	if schedule_idx_atoi_err != nil {
-		ctx.String(http.StatusBadRequest, "invalid 'schedule_idx' parameter value")
-		return
-	}
-
-	if schedule_idx < 0 {
-		ctx.String(http.StatusBadRequest, "university schedule underflow: class schedule does not exist")
-		return
-	}
-
-	if schedule_idx >= len(university_schedules) {
-		ctx.String(http.StatusBadRequest, "university schedule overflow: class schedule does not exist, university schedules might have been altered")
+	if !is_valid_idx {
 		return
 	}
 
--- Routes/RoutesV1/GetUniversitySchedule.go
+++ Routes/RoutesV1/GetUniversitySchedule.go
@@ -2,52 +2,24 @@ package RoutesV1
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/mrdcvlsc/scheduling-system-backend/RouteGlobals"
 	"github.com/mrdcvlsc/scheduling-system-backend/Schedule"
 )
 
 // GET:  /v1/university_schedule?semester=0  or  /v1/university_schedule?semester=1
 func GetUniversitySchedule(ctx *gin.Context) {
 
-	param_semester := ctx.Query("semester")
+	selected_semester, is_valid_semester_para := IsValidParameterSemesterIndex(ctx)
 
-	if param_semester == "" {
-		ctx.String(http.StatusBadRequest, "mising 'semester' parameter or parameter value")
+	if !is_valid_semester_para {
 		return
 	}
 
-	selected_semester, semester_atoi_err := strconv.Atoi(param_semester)
+	university_schedules, has_obtained := ObtainUniversitySchedule(ctx, selected_semester)
 
-	if semester_atoi_err != nil {
-		ctx.String(http.StatusBadRequest, "invalid 'semester' parameter value")
+	if !has_obtained {
 		return
-	}
-
-	if selected_semester < 0 || selected_semester >= 2 {
-		ctx.String(http.StatusBadRequest, "invalid 'semester' index value")
-		return
-	}
-
-	university_schedules, read_err := RouteGlobals.SchedulePersistence.ReaderService.LoadSchedules(selected_semester)
-
-	if read_err != nil {
-		ctx.String(http.StatusInternalServerError, "error reading schedule")
-		return
-	}
-
-	if university_schedules.IsEmpty() {
-		ctx.String(http.StatusNotFound, "schedule was not found, please generate one first")
-		return
-	}
-
-	for _, validation_err := range university_schedules.Validate() {
-		if validation_err != nil {
-			ctx.String(http.StatusConflict, "server detected an invalid schedule")
-			return
-		}
 	}
 
 	serialized_schedule := Schedule.SerializeUniversitySchedule(university_schedules)
