651a308be87c7f16694bbee2a59de332e1b3b9e5
mrdcvlsc <mrdcvlsc@gmail.com>
2025-03-04 17:21:50 +0800

    add instructor Post and Delete route

--- main.go
+++ main.go
@@ -134,6 +134,8 @@ func main() {
 	v1.GET("/instructors/d", RoutesV1.GetDepartmentInstructorsDefaults)
 	v1.GET("/instructors/a", RoutesV1.GetDepartmentInstructorsAllocated)
 	v1.PATCH("/instructor_update", RoutesV1.PatchInstructor)
+	v1.PATCH("/instructor_add", RoutesV1.PostInstructor)
+	v1.DELETE("/instructor_remove", RoutesV1.DeleteInstructor)
 
 	v1.GET("/university_schedule", RoutesV1.GetUniversitySchedule)
 	v1.POST("/university_schedule", RoutesV1.PostUniversitySchedule)
