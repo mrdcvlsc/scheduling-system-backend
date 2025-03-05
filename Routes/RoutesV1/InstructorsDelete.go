906642f61b4f42e19db6ea1acea3a7da0ce368ad
mrdcvlsc <mrdcvlsc@gmail.com>
2025-03-05 12:21:46 +0800

    implement error when trying to delete instructor that is assigned to a schedule

--- Routes/RoutesV1/InstructorsDelete.go
+++ Routes/RoutesV1/InstructorsDelete.go
@@ -1,10 +1,13 @@
 package RoutesV1
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mrdcvlsc/scheduling-system-backend/GeneticAlgorithm"
 	"github.com/mrdcvlsc/scheduling-system-backend/RouteGlobals"
+	"github.com/mrdcvlsc/scheduling-system-backend/Schedule"
 )
 
 /*
@@ -19,6 +22,44 @@ func DeleteInstructor(ctx *gin.Context) {
 		return
 	}
 
+	{ // check if instructor is assign in the first semester subjects
+		university_schedules, has_obtained := ObtainUniversitySchedule(ctx, nil, GeneticAlgorithm.TERM_1ST_SEMESTER)
+
+		if !has_obtained {
+			return
+		}
+
+		err_set_cache := RouteGlobals.SetCachedUniversitySchedule(GeneticAlgorithm.TERM_1ST_SEMESTER, university_schedules)
+
+		if err_set_cache != nil {
+			log.Println(err_set_cache.Error())
+		}
+
+		if is_instructor_assigned(university_schedules, uint16(instructor_id)) {
+			ctx.String(http.StatusConflict, "can not delete an instructor assigned to a schedule")
+			return
+		}
+	}
+
+	{ // check if instructor is assign in the second semester subjects
+		university_schedules, has_obtained := ObtainUniversitySchedule(ctx, nil, GeneticAlgorithm.TERM_2ND_SEMESTER)
+
+		if !has_obtained {
+			return
+		}
+
+		err_set_cache := RouteGlobals.SetCachedUniversitySchedule(GeneticAlgorithm.TERM_2ND_SEMESTER, university_schedules)
+
+		if err_set_cache != nil {
+			log.Println(err_set_cache.Error())
+		}
+
+		if is_instructor_assigned(university_schedules, uint16(instructor_id)) {
+			ctx.String(http.StatusConflict, "can not delete an instructor assigned to a schedule")
+			return
+		}
+	}
+
 	err := RouteGlobals.ResourcesPersistence.WriterService.DeleteInstructor(uint16(instructor_id))
 
 	if err != nil {
@@ -27,4 +68,18 @@ func DeleteInstructor(ctx *gin.Context) {
 	}
 
 	ctx.String(http.StatusOK, "instructor deleted successfully")
+}
+
+func is_instructor_assigned(university_schedules Schedule.UniTimeTables, instructor_id uint16) bool {
+	for _, section_week_schedules := range university_schedules {
+		for _, day_time_table := range section_week_schedules {
+			for _, time_slot := range day_time_table {
+				if instructor_id == time_slot.GetInstructorID() {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
