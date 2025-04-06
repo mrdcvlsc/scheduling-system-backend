e3028f07600ab085b31a1ad0192b366db82279e2
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-06 20:20:10 +0800

    implement new util function MidSectionSplitInSlice

--- Utils/SliceChunkOperations.go
+++ Utils/SliceChunkOperations.go
@@ -11,3 +11,14 @@ func RemoveChunkInSlice[T any](slice []T, start, size int) ([]T, error) {
 
 	return append(slice[:start], slice[end:]...), nil
 }
+
+// returns 3 slices, the left section, middle section, and right section of an original slice.
+func MidSectionSplitInSlice[T any](slice []T, start, size int) ([]T, []T, []T, error) {
+	end := start + size
+
+	if start < 0 || size < 0 || end > len(slice) {
+		return nil, nil, nil, fmt.Errorf("SliceMidSectionSplit invalid parameters: start=%d, size=%d, len=%d", start, size, len(slice))
+	}
+
+	return slice[:start], slice[start:end], slice[end:], nil
+}
