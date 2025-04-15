e360c2351ba3173529564ec9a73f1318f9a5e2ff
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-15 21:19:11 +0800

    small changes to fitness function

--- GeneticAlgorithm/FitnessFunction.go
+++ GeneticAlgorithm/FitnessFunction.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mrdcvlsc/scheduling-system-backend/Schedule"
 )
 
-const PREFERED_CLASS_HOUR_PER_DAY float64 = 7.0
+const PREFERED_MAX_CLASS_HOUR_PER_DAY float64 = 7.0
 
 /*
 * Output Range:
@@ -34,6 +34,7 @@ func MeasureFitnessBasic(uni_sched Schedule.UniTimeTables) float64 {
 		for day := range Const.N_WEEKLY_SCHOOL_DAYS {
 
 			has_class_after_5pm := false
+			has_time_for_lunch := false
 			total_hours := 0.0
 			continuous_hours := 0.0
 
@@ -46,17 +47,22 @@ func MeasureFitnessBasic(uni_sched Schedule.UniTimeTables) float64 {
 					total_hours += (1.0 / float64(Const.N_HOUR_TIME_SLOTS))
 					continuous_hours += (1.0 / float64(Const.N_HOUR_TIME_SLOTS))
 				} else {
-					// check if there's a break after 3 to 5 hour class
-					if continuous_hours >= 2.9 && continuous_hours <= 5.1 {
-						section_sched_fitness += 1.0
-					}
-
-					if continuous_hours > 5.1 {
-						section_sched_fitness -= 0.5
+					if continuous_hours > 4.5 {
+						section_sched_fitness -= 0.75
 					}
 
 					continuous_hours = 0.0
 				}
+
+				if time_slot >= 8 && time_slot <= 12 && uni_sched[i][day][time_slot].GetSubjectID() == 0 {
+					has_time_for_lunch = true
+				}
+			}
+
+			if has_time_for_lunch {
+				section_sched_fitness += 1.0
+			} else {
+				section_sched_fitness -= 2.0
 			}
 
 			if has_class_after_5pm {
@@ -64,14 +70,14 @@ func MeasureFitnessBasic(uni_sched Schedule.UniTimeTables) float64 {
 			}
 
 			if total_hours > 0.0 {
-				section_sched_fitness += reciprocal_distance(total_hours, PREFERED_CLASS_HOUR_PER_DAY)
+				section_sched_fitness += reciprocal_distance(total_hours, PREFERED_MAX_CLASS_HOUR_PER_DAY) * 1.5
 			} else {
-				section_sched_fitness += 0.75
+				section_sched_fitness += 0.5
 			}
 
 			// no class during saturday.
-			if total_hours <= 0.001 && day == Const.N_WEEKLY_SCHOOL_DAYS-1 {
-				section_sched_fitness += 1
+			if total_hours > 4 && day == Const.N_WEEKLY_SCHOOL_DAYS-1 {
+				section_sched_fitness -= 0.75
 			}
 		}
 
