006948fb72ad597adbf0e9976092a062ae36972e
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-17 15:07:31 +0800

    write TODO plan for crossover function

--- GeneticAlgorithm/Crossover.go
+++ GeneticAlgorithm/Crossover.go
@@ -1,3 +1,11 @@
 package GeneticAlgorithm
 
 // TODO: implement a crossover function
+
+// TODO: 1. just crossover the two parents
+
+// TODO: 2. check for overlapping vertical errors and horizontal errors
+
+// TODO: 3. delete the other genes or data that overlaps (pick a random gene/data that will remained)
+
+// TODO: 4. re-encode to complete the offspring's genome
