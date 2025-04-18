c9bd492c45f5fb8e6679cd2ae53aef40d2aa9933
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-18 22:59:17 +0800

    add semester block to uni sched iterator and updated arguments

--- GeneticAlgorithm/IndividualIterator.go
+++ GeneticAlgorithm/IndividualIterator.go
@@ -35,7 +35,8 @@ func IterateSectionsWeekSchedule(
 	curriculums []Curriculum.Curriculum,
 	selected_semester int,
 
-	fn_curriculum_block func(curriculum_idx int, curriculum *Curriculum.Curriculum) IterReturnType,
+	fn_curriculum_block func(indicies IterIndices, values IterValues) IterReturnType,
+	fn_semester_block func(indicies IterIndices, values IterValues) IterReturnType,
 
 	fn_section_block func(
 		indicies IterIndices, values IterValues,
@@ -47,7 +48,16 @@ curriculum_loop:
 	for curriculum_idx, curriculum := range curriculums {
 
 		if fn_curriculum_block != nil {
-			irt_curriculum_block := fn_curriculum_block(curriculum_idx, &curriculum)
+			irt_curriculum_block := fn_curriculum_block(
+				IterIndices{
+					Usi:        usi,
+					Curriculum: curriculum_idx,
+				},
+				IterValues{
+					Sched:      sched,
+					Curriculum: &curriculum,
+				},
+			)
 
 			switch irt_curriculum_block {
 			case IterBreakCurriculumLoop:
@@ -75,9 +85,44 @@ curriculum_loop:
 					continue // skip semesters that don't have subjects
 				}
 
+				if fn_semester_block != nil {
+					irt_semester_block := fn_semester_block(
+						IterIndices{
+							Usi:        usi,
+							Curriculum: curriculum_idx,
+							YearLevel:  year_level_idx,
+							Semester:   semester_idx,
+						},
+						IterValues{
+							Sched:      sched,
+							Curriculum: &curriculum,
+							YearLevel:  &year_level,
+							Semester:   &semester,
+						},
+					)
+
+					switch irt_semester_block {
+					case IterBreakCurriculumLoop:
+						break curriculum_loop
+					case IterContinue:
+						continue
+					case IterBreak:
+						break
+					}
+				}
+
 				for section_idx := 0; section_idx < semester.Sections; section_idx++ {
 
 					if fn_section_block != nil {
+
+						var week_time_table *Schedule.WeekTimeTable
+
+						if len(sched) > 0 {
+							week_time_table = &sched[usi]
+						} else {
+							week_time_table = nil
+						}
+
 						irt_section_block := fn_section_block(
 							IterIndices{
 								Usi:        usi,
@@ -86,10 +131,9 @@ curriculum_loop:
 								Semester:   semester_idx,
 								Section:    section_idx,
 							},
-
 							IterValues{
 								Sched:      sched,
-								WeekSched:  &sched[usi],
+								WeekSched:  week_time_table,
 								Curriculum: &curriculum,
 								YearLevel:  &year_level,
 								Semester:   &semester,
