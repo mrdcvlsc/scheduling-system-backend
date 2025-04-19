6d1da3be62054595103c2979cca418c91b039960
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-19 19:00:22 +0800

    increase node fetch wait length

--- Tests/schedule-serialization.js
+++ Tests/schedule-serialization.js
@@ -206,7 +206,7 @@ async function test(base_url = '') {
       }
     }
 
-    await new Promise(resolve => setTimeout(resolve, 10000));
+    await new Promise(resolve => setTimeout(resolve, 1000 * 60 * 8));
 
     console.log('--------------------fetch_serialized_schedule---------------------------\n')
     let [raw_data, _] = await fetch_serialized_schedule(0, base_url);
