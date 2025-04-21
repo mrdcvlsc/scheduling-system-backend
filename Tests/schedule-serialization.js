fc9b3ddf1f04b09fad2ab064884607ff3404f0bf
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-21 16:04:13 +0800

    add second semester and reruns to node fetch tests

--- Tests/schedule-serialization.js
+++ Tests/schedule-serialization.js
@@ -220,7 +220,7 @@ export async function getValidateSchedules(semesterIndex, departmentID, base_url
   return await response.text()
 }
 
-async function test(base_url = '') {
+async function test(base_url = '', semester) {
   try {
     console.log('--------------------fetch departments---------------------------\n')
 
@@ -230,7 +230,7 @@ async function test(base_url = '') {
 
     for (const department of departments) {
       if (department.DepartmentID > 0) {
-        await generate_schedule(0, department.DepartmentID, base_url)
+        await generate_schedule(semester, department.DepartmentID, base_url)
       }
     }
 
@@ -239,20 +239,20 @@ async function test(base_url = '') {
     console.log('-----------validate each departments one-by-one-----------------\n')
 
     for (const department of departments) {
-      const result = await getValidateSchedules(0, department.DepartmentID, base_url)
+      const result = await getValidateSchedules(semester, department.DepartmentID, base_url)
 
       console.log(`single validation ${department.DepartmentID} error :`)
       console.log(result)
     }
 
     console.log('--------------------fetch_serialized_schedule---------------------------\n')
-    let [raw_data, _] = await fetch_serialized_schedule(0, base_url);
+    let [raw_data, _] = await fetch_serialized_schedule(semester, base_url);
     console.log('--------------------deserialize_schedule---------------------------\n')
     let deserialized = await deserialize_schedule(raw_data, base_url);
     console.log('--------------------serialize_schedule---------------------------\n')
     let serialized = await serialize_schedule(deserialized, base_url);
     console.log('--------------------send_serialized_schedule---------------------------\n')
-    await send_serialized_schedule(0, serialized, base_url)
+    await send_serialized_schedule(semester, serialized, base_url)
     console.log('--------------------die---------------------------\n')
 
     await fetch(`${base_url}/die`, {
@@ -287,4 +287,15 @@ async function test(base_url = '') {
   }
 }
 
-test(domain)
+async function run_tests() {
+  console.log('===================================== TEST 1 =====================================')
+  await test(domain, 0)
+  console.log('===================================== TEST 2 =====================================')
+  await test(domain, 1)
+
+  console.log('===================================== TEST 3 =====================================')
+  test(domain, 1)
+  await test(domain, 0)
+}
+
+run_tests()
