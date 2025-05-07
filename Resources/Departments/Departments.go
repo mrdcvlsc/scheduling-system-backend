83285944b522e0ae879cfa5fe5ca54273f352a4c
mrdcvlsc <mrdcvlsc@gmail.com>
2025-05-07 17:46:14 +0800

    add omitempty struct tag to Department struct members

--- Resources/Departments/Departments.go
+++ Resources/Departments/Departments.go
@@ -1,7 +1,8 @@
 package Departments
 
 type Department struct {
-	DepartmentID uint16 `json:"DepartmentID" bson:"DepartmentID"`
-	Code         string `json:"Code" bson:"Code"`
-	Name         string `json:"Name" bson:"Name"`
+	DepartmentID         uint16 `json:"DepartmentID,omitempty" bson:"DepartmentID,omitempty"`
+	Code                 string `json:"Code" bson:"Code"`
+	Name                 string `json:"Name,omitempty" bson:"Name,omitempty"`
+	SaltedHashedPassword string `json:"SaltedHashedPassword,omitempty" bson:"SaltedHashedPassword,omitempty"`
 }
