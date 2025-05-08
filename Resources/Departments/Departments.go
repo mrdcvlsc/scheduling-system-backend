8bf662804774087895222de04f76b1b0ad9f36e2
mrdcvlsc <mrdcvlsc@gmail.com>
2025-05-08 16:11:28 +0800

    remove back the omitempty on old Department struct members

--- Resources/Departments/Departments.go
+++ Resources/Departments/Departments.go
@@ -1,8 +1,8 @@
 package Departments
 
 type Department struct {
-	DepartmentID         uint16 `json:"DepartmentID,omitempty" bson:"DepartmentID,omitempty"`
+	DepartmentID         uint16 `json:"DepartmentID" bson:"DepartmentID"`
 	Code                 string `json:"Code" bson:"Code"`
-	Name                 string `json:"Name,omitempty" bson:"Name,omitempty"`
+	Name                 string `json:"Name" bson:"Name"`
 	SaltedHashedPassword string `json:"SaltedHashedPassword,omitempty" bson:"SaltedHashedPassword,omitempty"`
 }
