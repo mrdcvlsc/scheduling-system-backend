2b5836e59607ab87b3ba07a7ee793c7fe56e5e0f
mrdcvlsc <mrdcvlsc@gmail.com>
2025-05-17 12:57:02 +0800

    rebuild university schedule index when deleting a department

--- Routes/RoutesV1/DepartmentDelete.go
+++ Routes/RoutesV1/DepartmentDelete.go
@@ -1,11 +1,15 @@
 package RoutesV1
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mrdcvlsc/scheduling-system-backend/Auth"
+	"github.com/mrdcvlsc/scheduling-system-backend/GeneticAlgorithm"
+	"github.com/mrdcvlsc/scheduling-system-backend/Resources/Curriculum"
 	"github.com/mrdcvlsc/scheduling-system-backend/RouteGlobals"
+	"github.com/mrdcvlsc/scheduling-system-backend/Schedule"
 )
 
 /*
@@ -32,6 +36,57 @@ func DeleteDepartment(ctx *gin.Context) {
 
 	if is_allowed := Auth.IsDepartmentAllowed(ctx, uint16(department_id)); !is_allowed {
 		return
+	}
+
+	all_curriculums, err_read_all_curriculums := RouteGlobals.ResourcesPersistence.ReaderService.ReadAllCurriculum()
+
+	if err_read_all_curriculums != nil {
+		log.Print("DeleteDepartment: [error curriculum reads] error reading all curriculums")
+		ctx.String(http.StatusInternalServerError, "we are unable retrieve the curriculums right now")
+		return
+	}
+
+	for selected_semester := range Curriculum.SUPPORTED_SEMESTERS {
+		log.Printf("DeleteDepartment: [re-index-semester] rebuilding university schedule index for the %s", Curriculum.SEMESTER_INDEX_NAME[selected_semester])
+
+		// obtain univesity schedules for each semester
+
+		new_university_schedule := make(Schedule.UniTimeTables, 0)
+		university_schedule, has_obtain := ObtainUniversityScheduleNoHorizontalValidation(ctx, selected_semester)
+
+		if !has_obtain {
+			return
+		}
+
+		log.Printf("DeleteDepartment: [read-not-modified] university schedule length for the %s : %d", Curriculum.SEMESTER_INDEX_NAME[selected_semester], len(university_schedule))
+
+		GeneticAlgorithm.IterateSectionsWeekSchedule(university_schedule, all_curriculums, selected_semester, nil, nil,
+			func(indicies GeneticAlgorithm.IterIndices, values GeneticAlgorithm.IterValues) GeneticAlgorithm.IterReturnType {
+				if values.Curriculum.DepartmentID != uint16(department_id) {
+					new_university_schedule = append(new_university_schedule, *values.WeekSched)
+				}
+
+				return GeneticAlgorithm.IterProceed
+			},
+		)
+
+		// save the new university schedules
+
+		err_save_schedules := RouteGlobals.SchedulePersistence.SaveService.SaveSchedules(new_university_schedule, selected_semester)
+
+		if err_save_schedules != nil {
+			log.Print("DeleteDepartment: error save schedule")
+			ctx.String(http.StatusInternalServerError, "we're unable to save the deletion of the curriculum from the university schedules right now")
+			return
+		}
+
+		log.Printf("DeleteDepartment: [saved-modified] university schedule length for the %s : %d", Curriculum.SEMESTER_INDEX_NAME[selected_semester], len(new_university_schedule))
+
+		err_set_cache := RouteGlobals.SetCachedUniversitySchedule(selected_semester, new_university_schedule)
+
+		if err_set_cache != nil {
+			log.Print("DeleteDepartment: the new university schedule was saved, but we're unable to cache it")
+		}
 	}
 
 	err := RouteGlobals.ResourcesPersistence.WriterService.DeleteDepartment(uint16(department_id))
