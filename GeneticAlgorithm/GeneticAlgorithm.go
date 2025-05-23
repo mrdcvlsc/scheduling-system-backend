e19d0318ecb29d5c356675bb3978280dff7b98d9
mrdcvlsc <mrdcvlsc@gmail.com>
2025-05-23 18:00:05 +0800

    debuggin: disable all random mutation function

--- GeneticAlgorithm/GeneticAlgorithm.go
+++ GeneticAlgorithm/GeneticAlgorithm.go
@@ -310,7 +310,7 @@ func RunGeneticAlgorithm(
 			// ApplyRandomDaySwapTimeSlots(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
 			// ApplyRandomSubjectDaySwap(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
 			// ApplyRandomSubjectTimeSlotNudge(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
-			ApplyRandomSubjectTimeSlotAndDayNudge(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
+			// ApplyRandomSubjectTimeSlotAndDayNudge(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
 
 			// repair broken genome after mutations
 
@@ -364,7 +364,7 @@ func RunGeneticAlgorithm(
 						// ApplyRandomDaySwapTimeSlots(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
 						// ApplyRandomSubjectDaySwap(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
 						// ApplyRandomSubjectTimeSlotNudge(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
-						ApplyRandomSubjectTimeSlotAndDayNudge(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
+						// ApplyRandomSubjectTimeSlotAndDayNudge(population[i].UniSched, population[i].Resources, curriculums, department_id, selected_semester)
 					}
 
 					continue
