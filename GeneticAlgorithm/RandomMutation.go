7bb2fb175979d1774e50cd40b1f0688b85621d23
mrdcvlsc <mrdcvlsc@gmail.com>
2025-05-24 12:36:51 +0800

    RAHHHHHH!

--- GeneticAlgorithm/RandomMutation.go
+++ GeneticAlgorithm/RandomMutation.go
@@ -418,9 +418,11 @@ func ApplyRandomSubjectTimeSlotNudge(
 			for i_ts := 0; i_ts < rnd_subject.TimeSlotSize; i_ts++ {
 
 				log.Printf(
-					"\n\n[%d] Room Availability = %t\n\n[%d] Inst Availability = %t\n\n",
-					i_ts, (id_to_room[rnd_subject.RoomID].GetTimeSlotClassCount(rnd_subject.Day, (rnd_subject.StartingTimeSlot+nudge_value+i_ts))) < uint8(id_to_room[rnd_subject.RoomID].Capacity),
-					i_ts, id_to_instructor[rnd_subject.InstructorID].Time.GetAvailability(rnd_subject.Day, rnd_subject.StartingTimeSlot+nudge_value+i_ts),
+					"\n\n[%d] Room Availability (%d, %d) = %t\n\n[%d] Inst Availability (%d, %d) = %t\n\n",
+					i_ts, rnd_subject.Day, (rnd_subject.StartingTimeSlot + nudge_value + i_ts),
+					(id_to_room[rnd_subject.RoomID].GetTimeSlotClassCount(rnd_subject.Day, (rnd_subject.StartingTimeSlot+nudge_value+i_ts))) < uint8(id_to_room[rnd_subject.RoomID].Capacity),
+					i_ts, rnd_subject.Day, rnd_subject.StartingTimeSlot+nudge_value+i_ts,
+					id_to_instructor[rnd_subject.InstructorID].Time.GetAvailability(rnd_subject.Day, rnd_subject.StartingTimeSlot+nudge_value+i_ts),
 				)
 
 				nudge_slot := sched[usi][rnd_subject.Day].GetTimeSlot(rnd_subject.StartingTimeSlot + nudge_value + i_ts)
@@ -443,6 +445,14 @@ func ApplyRandomSubjectTimeSlotNudge(
 				if next_room_val != prev_room_val+1 {
 					log.Panicf("increment is wrong for normal values: previous = %d, next = %d", prev_room_val, next_room_val)
 				}
+
+				log.Printf(
+					"\n\n[%d] Room Availability (%d, %d) = %t\n\n[%d] Inst Availability (%d, %d) = %t\n\n",
+					i_ts, rnd_subject.Day, (rnd_subject.StartingTimeSlot + nudge_value + i_ts),
+					(id_to_room[rnd_subject.RoomID].GetTimeSlotClassCount(rnd_subject.Day, (rnd_subject.StartingTimeSlot+nudge_value+i_ts))) < uint8(id_to_room[rnd_subject.RoomID].Capacity),
+					i_ts, rnd_subject.Day, rnd_subject.StartingTimeSlot+nudge_value+i_ts,
+					id_to_instructor[rnd_subject.InstructorID].Time.GetAvailability(rnd_subject.Day, rnd_subject.StartingTimeSlot+nudge_value+i_ts),
+				)
 
 				///////////////////
 
