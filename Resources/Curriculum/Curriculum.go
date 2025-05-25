c924331b620c06bb6be58d30739d4e870b17df7d
mrdcvlsc <mrdcvlsc@gmail.com>
2025-05-26 01:15:38 +0800

    increase supported section letters

--- Resources/Curriculum/Curriculum.go
+++ Resources/Curriculum/Curriculum.go
@@ -31,7 +31,18 @@ var SEMESTER_INDEX_NAME [6]string = [6]string{
 
 const SUPPORTED_SEMESTERS int = 3
 
-var SECTION [26]string = [26]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+var SECTION [62]string = [62]string{
+	// uppercase letters
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+
+	// lowercase letters
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+
+	// numbers
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+}
 
 func GetTotalNumberOfSections(curriculums []Curriculum, selected_semester int) int {
 	section_count := 0
